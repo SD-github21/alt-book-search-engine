721cada776ef69f09809e4eab9bde6a13ee6e6b8
SD-github21 <sapandonde.gmail.com>
2022-08-13 00:34:01 -0500

    refactored code to login and sign up

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,19 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-import { loginUser } from '../utils/API';
+
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -25,18 +30,16 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
+    // Submit form
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: { ...userFormData}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
+      console.log(data);
+      Auth.login(data.login.token);
+    } catch(e) {
+      console.error(e);
       setShowAlert(true);
     }
 
@@ -85,6 +88,7 @@ const LoginForm = () => {
           Submit
         </Button>
       </Form>
+      { error && <div>Login failed</div>}
     </>
   );
 };
